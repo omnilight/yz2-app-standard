2b6a6fe7b7031025d555195258a7f63957565cc9
Pavel Agalecky <pavel.agalecky@gmail.com>
2013-12-17 14:11:40 +0400

    Updated composer.json

--- app/vendor/autoload.php
+++ app/vendor/autoload.php
@@ -4,4 +4,4 @@
 
 require_once __DIR__ . '/composer' . '/autoload_real.php';
 
-return ComposerAutoloaderInit7c9df0afe2fead9b5b6061073a43b20a::getLoader();
+return ComposerAutoloaderInit617b053bcccc9b1a4225b1ec84735a70::getLoader();
--- app/vendor/yiisoft/extensions.php
+++ app/vendor/yiisoft/extensions.php
@@ -3,15 +3,6 @@
 $vendorDir = dirname(__DIR__);
 
 return array (
-  'omnilight/yz2-gii' => 
-  array (
-    'name' => 'omnilight/yz2-gii',
-    'version' => '9999999-dev',
-    'alias' => 
-    array (
-      '@yz/gii' => $vendorDir . '/omnilight/yz2-gii/yz/gii',
-    ),
-  ),
   'yiisoft/yii2-debug' => 
   array (
     'name' => 'yiisoft/yii2-debug',
@@ -19,15 +10,6 @@ return array (
     'alias' => 
     array (
       '@yii/debug' => $vendorDir . '/yiisoft/yii2-debug/yii/debug',
-    ),
-  ),
-  'yiisoft/yii2-bootstrap' => 
-  array (
-    'name' => 'yiisoft/yii2-bootstrap',
-    'version' => '9999999-dev',
-    'alias' => 
-    array (
-      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap/yii/bootstrap',
     ),
   ),
   'yiisoft/yii2-gii' => 
@@ -39,6 +21,34 @@ return array (
       '@yii/gii' => $vendorDir . '/yiisoft/yii2-gii/yii/gii',
     ),
   ),
+  'kartik-v/yii2-widgets' => 
+  array (
+    'name' => 'kartik-v/yii2-widgets',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@kartik' => $vendorDir . '/kartik-v/yii2-widgets/kartik',
+    ),
+  ),
+  'omnilight/yz2-gii' => 
+  array (
+    'name' => 'omnilight/yz2-gii',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yz/gii' => $vendorDir . '/omnilight/yz2-gii/yz/gii',
+    ),
+    'bootstrap' => 'yz\\admin\\Extension',
+  ),
+  'yiisoft/yii2-bootstrap' => 
+  array (
+    'name' => 'yiisoft/yii2-bootstrap',
+    'version' => '9999999-dev',
+    'alias' => 
+    array (
+      '@yii/bootstrap' => $vendorDir . '/yiisoft/yii2-bootstrap/yii/bootstrap',
+    ),
+  ),
   'omnilight/yz2' => 
   array (
     'name' => 'omnilight/yz2',
@@ -47,6 +57,7 @@ return array (
     array (
       '@yz' => $vendorDir . '/omnilight/yz2/yz',
     ),
+    'bootstrap' => 'yz\\admin\\Extension',
   ),
   'omnilight/yz2-admin' => 
   array (
@@ -56,14 +67,6 @@ return array (
     array (
       '@yz/admin' => $vendorDir . '/omnilight/yz2-admin/yz/admin',
     ),
-  ),
-  'kartik-v/yii2-widgets' => 
-  array (
-    'name' => 'kartik-v/yii2-widgets',
-    'version' => '9999999-dev',
-    'alias' => 
-    array (
-      '@kartik' => $vendorDir . '/kartik-v/yii2-widgets/kartik',
-    ),
+    'bootstrap' => 'yz\\admin\\Extension',
   ),
 );
