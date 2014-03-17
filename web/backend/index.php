3f7f64b52375b9f7b7d27f36379fda2a3d194329
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-03-17 22:45:21 +0400

    Fixes

--- web/backend/index.php
+++ web/backend/index.php
@@ -2,8 +2,18 @@
 defined('YII_ENV') || define('YII_ENV', getenv('APP_ENV')?:'prod');
 defined('YII_DEBUG') || define('YII_DEBUG', YII_ENV == 'dev');
 
-define('YZ_APP_DIR', __DIR__.'/../../app');
-define('YZ_VENDOR_DIR',__DIR__.'/../../app/vendor');
+switch (YII_ENV) {
+    case 'dev':
+        define('YZ_APP_DIR', __DIR__.'/../../app');
+        define('YZ_VENDOR_DIR',__DIR__.'/../../app/vendor');
+        break;
+    case 'prod':
+        define('YZ_APP_DIR', __DIR__.'/../../app');
+        define('YZ_VENDOR_DIR',__DIR__.'/../../app/vendor');
+        break;
+    default:
+        throw new Exception("Unknown YII_ENV: ".YII_ENV);
+}
 
 require(YZ_VENDOR_DIR . '/autoload.php');
 require(YZ_VENDOR_DIR . '/yiisoft/yii2/Yii.php');
