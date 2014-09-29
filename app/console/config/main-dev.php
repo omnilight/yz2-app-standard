3d6b8d8b70e1891b5dd5f618be6adb27a3a199b8
Pavel Agalecky <pavel.agalecky@gmail.com>
2014-09-29 20:13:14 +0400

    Added gii to the console

--- app/console/config/main-dev.php
+++ app/console/config/main-dev.php
@@ -1,3 +1,15 @@
 <?php
 
-return [];
+return [
+    'bootstrap' => ['gii'],
+    'modules' => [
+        'gii' => [
+            'class' => 'yii\gii\Module',
+            'generators' => [
+                'yz-model' => ['class' => 'yz\gii\generators\model\Generator'],
+                'yz-crud' => ['class' => 'yz\gii\generators\crud\Generator'],
+                'yz-module' => ['class' => 'yz\gii\generators\module\Generator'],
+            ],
+        ],
+    ]
+];
