e08268548b0e42eb2e94e86e75aa2059ddd957b0
Pavel <pavel.agalecky@gmail.com>
2015-06-13 10:09:16 +0300

    Version update and cleanup of bootstrap files

--- app/backend/config/bootstrap.php
+++ app/backend/config/bootstrap.php
@@ -1,8 +1,1 @@
 <?php
-// Environment specific settings
-switch (YII_ENV) {
-    case YII_ENV_DEV:
-        break;
-    case YII_ENV_PROD:
-        break;
-}
--- app/common/config/bootstrap.php
+++ app/common/config/bootstrap.php
@@ -1,10 +1,21 @@
 <?php
 
+/**
+ * Aliases configuration
+ *
+ * All aliases that are used in application are placed here
+ */
+
+// Aliases to applications
 Yii::setAlias('common', dirname(dirname(__DIR__)) . '/common');
 Yii::setAlias('frontend', dirname(dirname(__DIR__)) . '/frontend');
 Yii::setAlias('backend', dirname(dirname(__DIR__)) . '/backend');
 Yii::setAlias('console', dirname(dirname(__DIR__)) . '/console');
+
+// Modules
 Yii::setAlias('modules', dirname(dirname(__DIR__)) . '/modules');
+
+// Application migrations
 Yii::setAlias('migrations', dirname(dirname(__DIR__)) . '/migrations');
 
 // Data
@@ -15,3 +26,13 @@ Yii::setAlias('frontendWebroot', dirname(dirname(dirname(__DIR__))) . '/web/fron
 Yii::setAlias('backendWebroot', dirname(dirname(dirname(__DIR__))) . '/web/backend');
 Yii::setAlias('frontendWeb', getenv('FRONTEND_WEB'));
 Yii::setAlias('backendWeb', getenv('BACKEND_WEB'));
+
+
+/**
+ * Configuration of the dependency injector container (DI)
+ *
+ */
+
+
+// Example:
+//Yii::$container->set('acme\DummyInterface', 'acme\DummyClass');
